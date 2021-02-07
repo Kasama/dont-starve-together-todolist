bd4e89aa484e9e7ddc6a543e1d4598032a93a789
Kasama <robertoaall@gmail.com>
2021-02-07 15:27:08 -0300

    .

--- modmain.lua
+++ modmain.lua
@@ -6,7 +6,9 @@ local DST = GLOBAL.TheSim.GetGameID ~= nil and GLOBAL.TheSim:GetGameID() == "DST
 
 local function ConstrolsPostConstruct(self)
   self.custom_ui = self:AddChild(Text(GLOBAL.UIFONT, 24))
-  self.custom_ui:SetString("HELLO")
+  self.custom_ui:SetPosition(-83, 23)
+  self.custom_ui:SetString("HELLO HELLO")
+  self.custom_ui:Show()
   self.custom_ui:MoveToFront()
 end
 
