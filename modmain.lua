51a18eac0d39e8301c10e2c22f8f646170ae247a
Kasama <robertoaall@gmail.com>
2021-02-07 13:29:20 -0300

    .

--- modmain.lua
+++ modmain.lua
@@ -4,8 +4,9 @@ local Text = require "widgets/text"
 
 local DST = GLOBAL.TheSim.GetGameID ~= nil and GLOBAL.TheSim:GetGameID() == "DST"
 
-local function PostControls(self)
+local function ConstrolsPostConstruct(self)
   self.custom_ui = self:AddChild(Text(GLOBAL.UIFONT, "HELLO"))
   self.custom_ui:MoveToFront()
 end
 
+AddClassPostConstruct("widgets/controls", ConstrolsPostConstruct)
